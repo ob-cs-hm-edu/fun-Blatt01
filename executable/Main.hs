5745009315b09649d16fb2fe839b91d2e42a9b15
Oliver Braun <ob@obraun.net>
2015-03-06 18:30:29 +0100

    s/für/==/

--- executable/Main.hs
+++ executable/Main.hs
@@ -6,7 +6,7 @@ import           System.IO (BufferMode (NoBuffering), hSetBuffering, stdout)
 main :: IO ()
 main = do
   hSetBuffering stdout NoBuffering
-  putStr "Geben Sie bitte eine Zahl ein (0 für Ende): "
+  putStr "Geben Sie bitte eine Zahl ein (0 == Ende): "
   number <- readLn :: IO Double
   putStrLn ("square(" ++ show number ++ ") = " ++ show (square number))
   if number == 0
