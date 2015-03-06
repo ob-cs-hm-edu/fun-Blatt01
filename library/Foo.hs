70f90a0bfe91e2c4525f2f72b8f40c221e7af310
Oliver Braun <ob@obraun.net>
2015-03-06 11:35:50 +0100

    doctest

--- library/Foo.hs
+++ library/Foo.hs
@@ -1,7 +1,11 @@
 -- | A Foo module.
 module Foo (square) where
 
--- | Calculate the square of a number
+{- | Calculate the square of a number
+
+>>> square 5.0
+25.0
+-}
 square :: Num a
        => a -- ^ the number
        -> a -- ^ the square
